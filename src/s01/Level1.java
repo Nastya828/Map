87579b8fe0848ec4ed42948faaf156234564f379
Anastasiia Iakimova <anastasiiaiakimova@MacBook-Pro.local>
2022-06-24 15:13:22 +0300

    Level 1

---
+++ src/s01/Level1.java
@@ -0,0 +1,16 @@
+package s01;
+
+public class Level1 {
+    int squirrel(int N) {
+        int f = 1;
+        int x = 0;
+        for (int i = 1; i <= N; i++) {
+            f *= i;
+        }
+        while (f != 0) {
+            x = f % 10;
+            f /= 10;
+        }
+        return x;
+    }
+}
